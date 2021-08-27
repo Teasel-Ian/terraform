ba0e932a97959a84061f3e904fc40d6435953087
Ian Harris <teasel.ian@gmail.com>
2021-08-27 08:59:06 +0100

    Add an action

--- scripts/setup-gitub-secrets.sh
+++ scripts/setup-gitub-secrets.sh
@@ -1,5 +1,12 @@
-PROJECT=ian-harris-test01
-SA=terraform-rw@${PROJECT}.iam.gserviceaccount.com
+GITHUB_PROJECT=TrigpointingUK/terraform
+GITHUB_TOKEN=$(cat ../creds/github_pat.txt)
+GCP_PROJECT=ian-harris-test01
+GCP_SA=terraform-cicd@${GCP_PROJECT}.iam.gserviceaccount.com
 
-gcloud iam service-accounts keys create ../creds/${PROJECT}-cicdkey.yaml --iam-account=${SA}
+#gcloud iam service-accounts keys create ../creds/${GCP_PROJECT}-cicdkey.yaml --iam-account=${GCP_SA}
+
+curl \
+  -u username:${GITHUB_TOKEN} \
+  -H "Accept: application/vnd.github.v3+json" \
+  https://api.github.com/repos/${GITHUB_PROJECT}/actions/secrets
 
