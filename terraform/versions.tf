bfed50416bbdc46dc10758e9e9967ab9ce8c873e
Ian Harris <teasel.ian@gmail.com>
2021-08-30 16:08:37 +0100

    Remote state

--- terraform/versions.tf
+++ terraform/versions.tf
@@ -2,8 +2,12 @@ terraform {
   required_version = "1.0.5"
   backend "gcs" {
   }
+  required_providers {
+    google = {
+      version = "3.81.0"
+    }
+  }
 }
 
 provider "google" {
-  version = "3.81.0"
 }
