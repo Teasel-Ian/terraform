632b2f6b3d1fcf4d76ab4b82c239ef052b4181e7
Ian Harris <teasel.ian@gmail.com>
2021-08-30 14:13:33 +0100

    New key

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "auto-expire" {
-  name          = "tuk-testing-auto-expiring-bucket-03"
+  name          = "tuk-testing-auto-expiring-bucket-04"
   location      = "EU"
   force_destroy = true
 
