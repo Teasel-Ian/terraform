9b7b3562126a7936690306af998748b8f705027e
Ian Harris <teasel.ian@gmail.com>
2021-08-30 11:19:33 +0100

    Try some terraform

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 resource "google_storage_bucket" "auto-expire" {
-  name          = "tuk-testing-auto-expiring-bucket"
+  name          = "tuk-testing-auto-expiring-bucket-01"
   location      = "EU"
   force_destroy = true
 
